5746428168ab2a36fcf36ae8d1fc65c297eb688a
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-04 16:32:27 +0530

    First Commit

---
+++ src/main/java/com/theatre/reservation/TheatreReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.theatre.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheatreReservationBackendApplication.class, args);
+	}
+
+}
