d93a18c6553ac95a81ed27b8d29c0036947d46f2
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-04 16:47:11 +0530

    First Test

---
+++ src/main/java/com/theatre/reservation/TestController.java
@@ -0,0 +1,13 @@
+package com.theatre.reservation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Theatre Reservation Backend is running ✅";
+    }
+}
