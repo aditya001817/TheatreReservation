5405894e4e793dc01bcd66f765d82fea45cc0372
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-08 18:04:31 +0530

    updated

--- src/main/java/com/theatre/reservation/entity/User.java
+++ src/main/java/com/theatre/reservation/entity/User.java
@@ -1,5 +1,13 @@
 package com.theatre.reservation.entity;
 
-public class User {
+import com.theatre.reservation.enums.Role;
 
+public class User {
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private Role role;
+    private String password;
 }
