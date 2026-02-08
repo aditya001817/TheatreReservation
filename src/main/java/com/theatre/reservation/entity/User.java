e1839a33c6d0330594937a9006f75fbd75d6d908
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-08 18:16:10 +0530

    updated

--- src/main/java/com/theatre/reservation/entity/User.java
+++ src/main/java/com/theatre/reservation/entity/User.java
@@ -10,10 +10,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true, nullable = false)
     private String email;
     private String firstName;
     private String lastName;
+    @Column(unique = true, nullable = false)
     private String username;
+
+
     private Role role;
     private String password;
 }
