46ad0822504bcf4b59a4d1760d5acacd76c2ea2d
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-08 18:11:32 +0530

    updated

--- src/main/java/com/theatre/reservation/entity/User.java
+++ src/main/java/com/theatre/reservation/entity/User.java
@@ -1,8 +1,14 @@
 package com.theatre.reservation.entity;
 
 import com.theatre.reservation.enums.Role;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String firstName;
