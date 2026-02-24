49244aba98e5f94d678d1d49be24a8485a6ed913
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-24 20:06:09 +0530

    added to movie controller

--- src/main/java/com/theatre/reservation/controller/MovieController.java
+++ src/main/java/com/theatre/reservation/controller/MovieController.java
@@ -2,34 +2,40 @@ package com.theatre.reservation.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/movies/")
 
 public class MovieController {
 
+    @GetMapping("/all")
     public ResponseEntity<?> getAllMovies() {
         System.out.println("Getting all movies");
         return  null;
     }
 
+    @GetMapping("/movie/id/{id}")
     public ResponseEntity<?> getMovieById(int id) {
         System.out.println("Getting movie by id: " + id);
         return  null;
     }
 
+    @GetMapping("/movie/genre/{genre}")
     public ResponseEntity<?> getMovieByGenre(String genre) {
         System.out.println("Getting movie by genre: " + genre);
         return  null;
     }
 
+    @GetMapping("/movie/language/{language}")
     public ResponseEntity<?> getMovieByLanguage(String language) {
         System.out.println("Getting movie by language: " + language);
         return  null;
     }
 
+    @PostMapping("/create")
     public ResponseEntity<?> createMovie() {
         System.out.println("Creating movie");
         return null;
