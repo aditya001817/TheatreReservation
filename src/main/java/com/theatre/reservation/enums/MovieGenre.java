db5ccad87fd16786ba200afc8c196cc0bf80cdeb
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-25 15:43:35 +0530

    added movieGenre

--- src/main/java/com/theatre/reservation/enums/MovieGenre.java
+++ src/main/java/com/theatre/reservation/enums/MovieGenre.java
@@ -1,4 +1,12 @@
 package com.theatre.reservation.enums;
 
 public enum MovieGenre {
+    ACTION,
+    SCIENCE_FICTION,
+    COMEDY,
+    THRILLER,
+    HORROR,
+    ROMANTIC,
+    SUSPENSE,
+    DRAMA
 }
