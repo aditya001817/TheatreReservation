3a47c76f496671a0ed9d1b98ac261242b46745a1
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-27 19:22:38 +0530

    added to theater controller

--- src/main/java/com/theatre/reservation/controller/TheaterController.java
+++ src/main/java/com/theatre/reservation/controller/TheaterController.java
@@ -8,32 +8,39 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/theaters")
 public class TheaterController {
 
+
+    @GetMapping("/theater/all")
     public ResponseEntity<?> getAllTheaters(@RequestParam(defaultValue = "0") int page,
                                             @RequestParam(defaultValue = "3") int size) {
         System.out.println("Getting all Theaters");
         return null;
     }
 
+    @GetMapping("/theater/location/{location}")
     public ResponseEntity<?> getAllTheaterByLocation(@PathVariable String location) {
         System.out.println("Getting theater by location"+location);
         return null;
     }
 
+    @GetMapping("/theater/id/{id}")
     public ResponseEntity<?> getTheaterById(@PathVariable long theaterId) {
         System.out.println("Getting theater by id"+theaterId);
         return null;
     }
 
+    @PostMapping("/theater/create")
     public ResponseEntity<?> createTheater(@RequestBody TheaterRequestDto theaterRequestDto) {
         System.out.println("Creating theater");
         return null;
     }
 
+    @PutMapping("/theater/update/{id}")
     public ResponseEntity<?> updateTheaterById(@PathVariable long id, @RequestBody TheaterRequestDto theaterRequestDto) {
         System.out.println("Updating theater by id"+id);
         return null;
     }
 
+    @DeleteMapping("/theater/delete/{id}")
     public ResponseEntity<?> deleteTheaterById(@PathVariable long id) {
         System.out.println("Deleting theater by id"+id);
         return null;
