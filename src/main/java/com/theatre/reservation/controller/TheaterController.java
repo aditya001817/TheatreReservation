dfbaf6bb2966696cb65f303f5b27fd7e557529f0
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-27 15:53:53 +0530

    added to theater controller

--- src/main/java/com/theatre/reservation/controller/TheaterController.java
+++ src/main/java/com/theatre/reservation/controller/TheaterController.java
@@ -1,10 +1,16 @@
 package com.theatre.reservation.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/theaters")
 public class TheaterController {
+
+    public ResponseEntity<?> getAllTheaters() {
+        System.out.println("Getting all Theaters");
+        return null;
+    }
 }
