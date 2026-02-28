54f3360f879249407fdfed2ba2b99c08784c97f1
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-02-28 16:59:18 +0530

    added to theater controller

--- src/main/java/com/theatre/reservation/controller/TheaterController.java
+++ src/main/java/com/theatre/reservation/controller/TheaterController.java
@@ -3,13 +3,25 @@ package com.theatre.reservation.controller;
 
 import com.theatre.reservation.dto.ApiResponseDto;
 import com.theatre.reservation.dto.TheaterRequestDto;
+import com.theatre.reservation.service.TheaterService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Setter
+@Getter
 @RestController
 @RequestMapping("/api/v1/theaters")
 public class TheaterController {
 
+    private TheaterService theaterService;
+
+    @Autowired
+    public TheaterController(TheaterService theaterService) {
+        this.theaterService = theaterService;
+    }
 
     @GetMapping("/theater/all")
     public ResponseEntity<?> getAllTheaters(@RequestParam(defaultValue = "0") int page,
@@ -49,4 +61,5 @@ public class TheaterController {
         System.out.println("Deleting theater by id"+id);
         return null;
     }
+
 }
