80a70e04e6260a5091d376509c632c90eb5794c9
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-03-02 19:21:14 +0530

    added to Theater

--- src/main/java/com/theatre/reservation/entity/Theater.java
+++ src/main/java/com/theatre/reservation/entity/Theater.java
@@ -4,8 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
 public class Theater {
 
     @Id
