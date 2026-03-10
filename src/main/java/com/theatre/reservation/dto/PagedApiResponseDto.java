386828a74b9d5a69213b81136a8350fde7dfe304
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-03-10 20:41:05 +0530

    added pagedApiresponseDto

--- src/main/java/com/theatre/reservation/dto/PagedApiResponseDto.java
+++ src/main/java/com/theatre/reservation/dto/PagedApiResponseDto.java
@@ -1,4 +1,14 @@
 package com.theatre.reservation.dto;
 
+import com.theatre.reservation.entity.Movie;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
 public class PagedApiResponseDto {
+
+    int totalPages;
+    long totalElements;
+    List<?> currentPageData;
+    int currentCount;
 }
