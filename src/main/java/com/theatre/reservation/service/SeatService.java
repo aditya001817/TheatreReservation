564242fa5c14398e1e6b703cd498b0d84f346e18
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-13 10:55:12 +0530

    updated seat service

--- src/main/java/com/theatre/reservation/service/SeatService.java
+++ src/main/java/com/theatre/reservation/service/SeatService.java
@@ -1,16 +1,23 @@
 package com.theatre.reservation.service;
 
+import com.theatre.reservation.entity.Seat;
 import com.theatre.reservation.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SeatService {
 
-    @Autowired
     private final SeatRepository seatRepository;
 
+    @Autowired
     public SeatService(SeatRepository seatRepository) {
         this.seatRepository = seatRepository;
+    }
+
+    public List<Seat> createSeatWithGivenPrice(int seats, double price, String area){
+        return null;
     }
 }
