61844706ea6dd5c46aed26d1c4fed71ae0fecc89
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-13 13:53:26 +0530

    updated show controller

--- src/main/java/com/theatre/reservation/controller/ShowController.java
+++ src/main/java/com/theatre/reservation/controller/ShowController.java
@@ -2,7 +2,9 @@ package com.theatre.reservation.controller;
 
 import com.theatre.reservation.service.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +14,12 @@ public class ShowController {
     private final ShowService showService;
 
     @Autowired
-    public ShowController(ShowService showService) {
-        this.showService = showService;
+    public ShowController(ShowService showService1) {
+        this.showService = showService1;
+    }
+
+    public ResponseEntity<?> getAllShows(@RequestParam(defaultValue = "0") int page,
+                                         @RequestParam(defaultValue = "10") int size) {
+        return null;
     }
 }
