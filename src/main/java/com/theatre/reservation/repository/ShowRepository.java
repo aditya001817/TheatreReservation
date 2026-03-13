02f856c8b609263a7d758db0545d581a9587c32b
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-13 13:30:55 +0530

    updated show repo

--- src/main/java/com/theatre/reservation/repository/ShowRepository.java
+++ src/main/java/com/theatre/reservation/repository/ShowRepository.java
@@ -2,6 +2,8 @@ package com.theatre.reservation.repository;
 
 import com.theatre.reservation.entity.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShowRepository extends JpaRepository<Show, Long> {
 }
