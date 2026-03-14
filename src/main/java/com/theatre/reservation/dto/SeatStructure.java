a27acfcd4458a4e458f70a8a25115e41770ae72c
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-14 21:34:02 +0530

    added seat structure and show request dto

---
+++ src/main/java/com/theatre/reservation/dto/SeatStructure.java
@@ -0,0 +1,10 @@
+package com.theatre.reservation.dto;
+
+import lombok.Data;
+
+@Data
+public class SeatStructure {
+    int seatCount;
+    double seatPrice;
+    String area;
+}
--- src/main/java/com/theatre/reservation/dto/ShowRequestDto.java
+++ src/main/java/com/theatre/reservation/dto/ShowRequestDto.java
@@ -1,4 +1,17 @@
 package com.theatre.reservation.dto;
 
+import com.theatre.reservation.entity.Seat;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class ShowRequestDto {
+    long movieId;
+    long theaterId;
+    String startTime;
+    String endTime;
+    List<SeatStructure> seats;
 }
