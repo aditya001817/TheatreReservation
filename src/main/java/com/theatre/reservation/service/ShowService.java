fc2b85926adf3bcf538ac5f0732f49edb1a1d9d1
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-19 23:23:40 +0530

    created createShow method in show service

--- src/main/java/com/theatre/reservation/service/ShowService.java
+++ src/main/java/com/theatre/reservation/service/ShowService.java
@@ -4,7 +4,9 @@ import com.theatre.reservation.dto.SeatStructure;
 import com.theatre.reservation.dto.ShowRequestDto;
 import com.theatre.reservation.entity.Seat;
 import com.theatre.reservation.entity.Show;
+import com.theatre.reservation.exception.MovieNotFoundException;
 import com.theatre.reservation.exception.ShowNotFoundException;
+import com.theatre.reservation.exception.TheaterNotFoundException;
 import com.theatre.reservation.repository.MovieRepository;
 import com.theatre.reservation.repository.SeatRepository;
 import com.theatre.reservation.repository.ShowRepository;
@@ -15,10 +17,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.theatre.reservation.constant.ExceptionMessages.SHOW_NOT_FOUND;
+import static com.theatre.reservation.constant.ExceptionMessages.*;
 
 @Service
 public class ShowService {
@@ -26,11 +29,11 @@ public class ShowService {
     private final ShowRepository showRepository;
     private final MovieRepository movieRepository;
     private final TheaterRepository theaterRepository;
-    private final SeatRepository seatRepository;
+    private final SeatService seatService;
 
     @Autowired
-    public ShowService(ShowRepository showRepository, MovieRepository movieRepository, TheaterRepository theaterRepository, SeatRepository seatRepository) {
-        this.seatRepository = seatRepository;
+    public ShowService(ShowRepository showRepository, MovieRepository movieRepository, TheaterRepository theaterRepository, SeatService seatService) {
+        this.seatService = seatService;
         this.showRepository = showRepository;
         this.movieRepository = movieRepository;
         this.theaterRepository = theaterRepository;
@@ -65,7 +68,26 @@ public class ShowService {
                 .map(movie -> theaterRepository.findById(showRequestDto.getTheaterId())
                         .map(theater -> {
                             List<Seat> seats = new ArrayList<>();
-                        }))
+                            showRequestDto.getSeats()
+                                    .forEach(seatStructure ->
+                                            seats.addAll(
+                                                    seatService.createSeatWithGivenPrice(
+                                                            seatStructure.getSeatCount(),
+                                                            seatStructure.getSeatPrice(),
+                                                            seatStructure.getArea()
+                                                    )
+                                            ));
+                            Show show = Show.builder()
+                                    .movie(movie)
+                                    .theater(theater)
+                                    .startTime(LocalDateTime.parse(showRequestDto.getStartTime()))
+                                    .endTime(LocalDateTime.parse(showRequestDto.getEndTime()))
+                                    .seats(seats)
+                                    .build();
+                            return showRepository.save(show);
+                        })
+                        .orElseThrow(() -> new TheaterNotFoundException(THEATER_NOT_FOUND, HttpStatus.BAD_REQUEST)))
+                .orElseThrow(() -> new MovieNotFoundException(MOVIE_NOT_FOUND, HttpStatus.BAD_REQUEST));
     }
 
     public void deleteShowById(long showId) {
