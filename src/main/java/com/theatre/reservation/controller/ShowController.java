f416fb3280bc0d82ff19706c0ae583c70844169f
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-20 22:44:21 +0530

    updated show controller

--- src/main/java/com/theatre/reservation/controller/ShowController.java
+++ src/main/java/com/theatre/reservation/controller/ShowController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RequestMapping("/api/v1/shows")
 public class ShowController {
 
-    private ShowService showService;
+    private final ShowService showService;
 
     @Autowired
     public ShowController(ShowService showService1) {
