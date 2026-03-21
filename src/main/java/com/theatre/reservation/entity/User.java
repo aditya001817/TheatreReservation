5e600a58d8cf1f6b0986bd210f3e2fea6a8d236c
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-21 19:04:49 +0530

    added spring security and updated user entity

--- src/main/java/com/theatre/reservation/entity/User.java
+++ src/main/java/com/theatre/reservation/entity/User.java
@@ -7,16 +7,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     long userId;
     String firstName;
     String lastName;
@@ -26,4 +32,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     Role role;
     String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 }
