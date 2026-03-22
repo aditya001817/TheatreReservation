841bf977f89af9d43067f6b7faddfbdd420b3294
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-22 21:25:10 +0530

    added mapping in user controller

--- src/main/java/com/theatre/reservation/controller/UserController.java
+++ src/main/java/com/theatre/reservation/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.theatre.reservation.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @GetMapping("/user/me")
     public ResponseEntity<?> currentUser() {
         System.out.println("Getting current user");
         return null;
