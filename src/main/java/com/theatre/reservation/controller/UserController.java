f3cea49783723a34c7e0016c739751c276c92c40
aditya001817 <aditya.chauhan00018@gmail.com>
2026-03-22 23:26:04 +0530

    added mapping in user controller

--- src/main/java/com/theatre/reservation/controller/UserController.java
+++ src/main/java/com/theatre/reservation/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.theatre.reservation.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -22,6 +19,7 @@ public class UserController {
         return null;
     }
 
+    @PutMapping("/promote/{username)")
     public ResponseEntity<?> promoteUserToAdmin(@PathVariable String username) {
         System.out.println("Promoting user "+username);
         return null;
