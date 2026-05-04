b49d88f91affb68e917648cb8eb6ecf6377faaa4
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-04 10:42:33 +0530

    worked on Seat service

--- src/main/java/com/theatre/reservation/service/SeatService.java
+++ src/main/java/com/theatre/reservation/service/SeatService.java
@@ -30,6 +30,6 @@ public class SeatService {
                     .status(SeatStatus.UNBOOKED)
                     .build());
         }
-        return seats;
+        return seatRepository.saveAll(seats);
     }
 }
