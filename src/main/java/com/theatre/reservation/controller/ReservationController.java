1ea02002bb0b06469fdb774cef6c55ec186016a5
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-06 19:07:42 +0530

    added reservation controller

---
+++ src/main/java/com/theatre/reservation/controller/ReservationController.java
@@ -0,0 +1,11 @@
+package com.theatre.reservation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reservations")
+public class ReservationController {
+
+
+}
