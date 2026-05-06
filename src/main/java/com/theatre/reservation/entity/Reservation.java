3d16d9d48626fa9cfbacbb69e22b1e44bfb88d4b
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-06 10:53:11 +0530

    added Reservation

---
+++ src/main/java/com/theatre/reservation/entity/Reservation.java
@@ -0,0 +1,35 @@
+package com.theatre.reservation.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne
+    @ToString.Exclude
+    private User user;
+
+    @ManyToOne
+    @ToString.Exclude
+    private Show show;
+
+    @ManyToOne
+    @ToString.Exclude
+    List<Seat> seatsReserved;
+    double amountPaid;
+
+    @Enumerated(value = EnumType.STRING)
+    private ReservationStatus reservationStatus;
+    private LocalDateTime createdAt;
+}
