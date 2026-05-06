012236a96670d5e1f8ffe65382e85df04cf7bc19
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-06 19:05:46 +0530

    added reservation request dto

---
+++ src/main/java/com/theatre/reservation/dto/ReservationRequestDto.java
@@ -0,0 +1,16 @@
+package com.theatre.reservation.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class ReservationRequestDto {
+
+    private long showId;
+    private List<Long> seatIdsReserve;
+    private double amount;
+
+}
