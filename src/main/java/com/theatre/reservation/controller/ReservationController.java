414072d05c628ae7a085ced82cb2c27f88a46602
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-07 10:29:24 +0530

    updated reservation controller

--- src/main/java/com/theatre/reservation/controller/ReservationController.java
+++ src/main/java/com/theatre/reservation/controller/ReservationController.java
@@ -1,11 +1,24 @@
 package com.theatre.reservation.controller;
 
+import com.theatre.reservation.dto.ApiResponseDto;
+import com.theatre.reservation.dto.ReservationRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/reservations")
 public class ReservationController {
 
+    public ResponseEntity<ApiResponseDto> getAllReservationsForCurrentUser(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+        System.out.println("Getting all reservations for current User ");
+        return null;
+    }
 
+    public ResponseEntity<ApiResponseDto> createReservation(@RequestParam ReservationRequestDto reservationRequestDto) {
+        System.out.println("Creating new Reservation");
+        return null;
+    }
 }
