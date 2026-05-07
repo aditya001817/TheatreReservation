cd05d33fa1d9a6dfa17666f4f0022078399438c9
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-07 11:21:01 +0530

    updated reservation service

--- src/main/java/com/theatre/reservation/service/ReservationService.java
+++ src/main/java/com/theatre/reservation/service/ReservationService.java
@@ -1,11 +1,18 @@
 package com.theatre.reservation.service;
 
+import com.theatre.reservation.entity.Reservation;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReservationService {
 
     public void cancelReservation(long reservationId) {
 
+    }
+
+    public List<Reservation> getAllReservationsForCurrentUser(int page, int size) {
+        return null;
     }
 }
