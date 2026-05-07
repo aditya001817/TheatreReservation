22a2a1816d124588fab02e85621ec69e8b742926
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-07 10:49:40 +0530

    added mappings to Reservation controller

--- src/main/java/com/theatre/reservation/controller/ReservationController.java
+++ src/main/java/com/theatre/reservation/controller/ReservationController.java
@@ -3,10 +3,7 @@ package com.theatre.reservation.controller;
 import com.theatre.reservation.dto.ApiResponseDto;
 import com.theatre.reservation.dto.ReservationRequestDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -14,16 +11,19 @@ import java.time.LocalDateTime;
 @RequestMapping("/api/v1/reservations")
 public class ReservationController {
 
+    @GetMapping("/user/all")
     public ResponseEntity<ApiResponseDto> getAllReservationsForCurrentUser(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
         System.out.println("Getting all reservations for current User ");
         return null;
     }
 
+    @PostMapping("/reserve")
     public ResponseEntity<ApiResponseDto> createReservation(@RequestParam ReservationRequestDto reservationRequestDto) {
         System.out.println("Creating new Reservation");
         return null;
     }
 
+    @GetMapping("/filter")
     public ResponseEntity<ApiResponseDto> filterReservation(@RequestParam(required = false) long movieId,
                                                             @RequestParam(required = false) long theaterId,
                                                             @RequestParam(required = false) long userId,
@@ -33,11 +33,13 @@ public class ReservationController {
         return  null;
     }
 
+    @GetMapping("/reservation/{reservationId}")
     public ResponseEntity<ApiResponseDto> getReservationById(@PathVariable long reservationId) {
         System.out.println("Getting Reservation by id "+reservationId);
         return null;
     }
 
+    @DeleteMapping("/cancel/{reservationId}")
     public ResponseEntity<ApiResponseDto> cancelReservation(@PathVariable long reservationId) {
         System.out.println("Canceling Reservation for ReservationId" +reservationId);
         return null;
