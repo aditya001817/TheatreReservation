1c0f4840d8eb599b67f108a7e1ee984685f68750
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-08 10:54:13 +0530

    updated seat lock manager

--- src/main/java/com/theatre/reservation/service/SeatLockManager.java
+++ src/main/java/com/theatre/reservation/service/SeatLockManager.java
@@ -3,8 +3,14 @@ package com.theatre.reservation.service;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Service
 @Scope("Singleton")
 public class SeatLockManager {
 
+    private ConcurrentHashMap<Long, ReentrantLock> seatLocks = new ConcurrentHashMap<>();
+
+    public
 }
