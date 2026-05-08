9a8338ea98d277b3c8bfe745879b1ba92901dca1
aditya001817 <aditya.chauhan00018@gmail.com>
2026-05-08 10:52:14 +0530

    added seat lock manager

---
+++ src/main/java/com/theatre/reservation/service/SeatLockManager.java
@@ -0,0 +1,10 @@
+package com.theatre.reservation.service;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("Singleton")
+public class SeatLockManager {
+
+}
